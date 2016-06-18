9c1771a22ad304e00bc7587fdf60e4e340488313
Wendell Thompson <jack0817@yahoo.com>
2016-06-17 22:35:33 -0400

    Initial Commit

---
+++ DynamicHeaderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicHeaderApp
+//
+//  Created by Wendell Thompson on 6/17/16.
+//  Copyright © 2016 Wendell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
